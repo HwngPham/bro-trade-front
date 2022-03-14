969e5a2e186dcc45c934cf370e23d0ae7bb44d79
hungpq96 <quanghwng22@gmail.com>
2022-03-14 11:35:39 +0700

    Login boilderplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginButton />
+      <Profile />
     </div>
-  );
+  )
 }
 
-export default App;
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0()
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>
+}
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0()
+
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
+
+  return isAuthenticated ? (
+    <div>
+      <img src={user?.picture} alt={user?.name} />
+      <h2>{user?.name}</h2>
+      <p>{user?.email}</p>
+    </div>
+  ) : null
+}
+
+export default App
